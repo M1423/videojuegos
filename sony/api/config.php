7f54e5b777b9efb060f70af28b9de52fe1a4b2c4
M1423 <maximilianotomazoni@institutocedec.com>
2024-11-12 15:48:28 -0300

    Add files via upload

---
+++ sony/api/config.php
@@ -0,0 +1,8 @@
+<?php
+    /* Definimos las constantes de conexión */
+    define('DB_HOST','localhost');   // Servidor MySQL
+    define('DB_USER','root');        // Usuario del servidor MySQUL
+    define('DB_PASS','');         // Cnostraseña de servidor MySQL
+    define('DB_NAME','gestionvideojuegos');        // Nombre de la BD
+    define('DB_CHARSET','utf8');          // Contejamiento conjunto de caracteres
+?>
